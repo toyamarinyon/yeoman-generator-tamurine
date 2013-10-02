afb07abce6bed6a2af0da2a8a862eef21e0fe9b2
tamurine <tamurine@TamurineMBA.local>
2013-10-02 13:38:14 +0900

    first commit

---
+++ app/index.js
@@ -0,0 +1,50 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var TamurineGenerator = module.exports = function TamurineGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(TamurineGenerator, yeoman.generators.Base);
+
+TamurineGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.someOption = props.someOption;
+
+    cb();
+  }.bind(this));
+};
+
+TamurineGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/templates');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
+
+TamurineGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
