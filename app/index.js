aaebc51987b9130447189d6c87208997cac6e92e
tamurine <tamurine@TamurineMBA.local>
2013-10-03 14:48:25 +0900

    study template method

--- app/index.js
+++ app/index.js
@@ -3,6 +3,21 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 
+var GitHubApi = require('github');
+var github = new GitHubApi({
+  version: '3.0.0'
+});
+
+var githubUserInfo = function (name, cb) {
+  github.user.getFrom({
+    user: name
+  }, function (err, res) {
+    if (err) {
+      throw err;
+    }
+    cb(JSON.parse(JSON.stringify(res)));
+  });
+};
 
 var TamurineGenerator = module.exports = function TamurineGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
@@ -20,19 +35,40 @@ TamurineGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
   // have Yeoman greet the user.
-  console.log(this.yeoman);
+  // console.log(this.yeoman);
 
   var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+    name: 'appName',
+    message: 'What do you want to call your application',
+	default: 'AwesomeWebApplication'
+  }, {
+    name: 'appDescription',
+    message: 'Please give me a description of the application.',
+	default: 'My Awesome Web Application!'
+  }, {
+    name: 'githubUser',
+    message: 'Would you mind telling me your username on Github?',
+    default: 'someuser'
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.appName = props.appName;
+    this.appDescription = props.appDescription;
+    this.githubUser = props.githubUser;
 
     cb();
+  }.bind(this));
+};
+
+TamurineGenerator.prototype.userInfo = function userInfo() {
+  var done = this.async();
+
+  githubUserInfo(this.githubUser, function (res) {
+    /*jshint camelcase:false */
+    this.realname = res.name;
+    this.email = res.email;
+    this.githubUrl = res.html_url;
+    done();
   }.bind(this));
 };
 
@@ -40,11 +76,5 @@ TamurineGenerator.prototype.app = function app() {
   this.mkdir('app');
   this.mkdir('app/templates');
 
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
-};
-
-TamurineGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+  this.template('configuration/package.json', 'package.json');
 };
